b347a671b8882ef3241026509e7a929d38f247cc
DELL <anjugupta7060@gmail.com>
2024-03-04 22:13:11 +0530

    Screen Development

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(padding: EdgeInsets.fromLTRB(40, 1.2*kToolbarHeight, 40, 20),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Align(
+              alignment: AlignmentDirectional(3, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ),
+            Align(
+              alignment: AlignmentDirectional(-3, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff673AB7),
+                ),
+              ),
+            ),
+            Align(
+              alignment: AlignmentDirectional(0, -1.2),
+              child: Container(
+                height: 300,
+                width: 600,
+                decoration: BoxDecoration(
+                  color: Color(0xffffab40),
+                ),
+              ),
+            ),
+            BackdropFilter(filter: ImageFilter.blur(sigmaX: 100,sigmaY: 100),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+              ),
+            ),),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("📍 Aix-en-Provenence",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                  ),),
+                  const SizedBox(height: 8,),
+                  Text("Good Morning",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),),
+    );
+  }
+}
