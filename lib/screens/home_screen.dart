6c1f5778e486af558ffb4db6abd671f828a1cce8
DELL <anjugupta7060@gmail.com>
2024-03-05 22:07:23 +0530

    Home screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../widget/sizedbox.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -17,73 +19,165 @@ class HomeScreen extends StatelessWidget {
         systemOverlayStyle:
             SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
       ),
-      body: Padding(padding: EdgeInsets.fromLTRB(40, 1.2*kToolbarHeight, 40, 20),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            Align(
-              alignment: AlignmentDirectional(3, -0.3),
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.deepPurple,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: AlignmentDirectional(-3, -0.3),
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xff673AB7),
+              Align(
+                alignment: AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xff673AB7),
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: AlignmentDirectional(0, -1.2),
-              child: Container(
-                height: 300,
-                width: 600,
-                decoration: BoxDecoration(
-                  color: Color(0xffffab40),
+              Align(
+                alignment: AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: BoxDecoration(
+                    color: Color(0xffffab40),
+                  ),
                 ),
               ),
-            ),
-            BackdropFilter(filter: ImageFilter.blur(sigmaX: 100,sigmaY: 100),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                ),
               ),
-            ),),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("📍 Aix-en-Provenence",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w300,
-                  ),),
-                  const SizedBox(height: 8,),
-                  Text("Good Morning",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "📍 Aix-en-Provenence",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    height8,
+                    Text(
+                      "Good Morning",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Image.asset("assets/images/img1.png"),
+                    Center(
+                      child: Text(
+                        "21°C",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 55,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "THUNDERSTROM",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    height4,
+                    Center(
+                      child: Text(
+                        "Friday 16, 9:41Jan",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                    height32,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        belowTimingRow("assets/images/img6.png","Sunrise","5:34 am"),
+                         belowTimingRow("assets/images/img9.png","Sunset","5:34 pm"),
+                      ],
+                    ),
+                    height4,
+                    Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        belowTimingRow("assets/images/img10.png","Temp Max","32°C"),
+                         belowTimingRow("assets/images/img12.png","Temp Min","8°C"),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),),
+      ),
     );
+  }
+
+  Widget belowTimingRow(String img, String heading, String time) {
+    return Row(
+                        children: [
+                          Image.asset(
+                            img,
+                            scale: 8,
+                          ),
+                          width4,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                heading,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                              height4,
+                              Text(
+                                time,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
   }
 }
